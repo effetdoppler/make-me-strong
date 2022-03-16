6733d801948f92968bb794a932462285a18cd97f
Michel ZHANG <michel529020@gmail.com>
2022-03-16 18:43:14 +0100

    add test utilities

---
+++ test.py
@@ -0,0 +1,8 @@
+from login_makeMeStrong import *
+from testlib import *
+
+if __name__ == '__main__':
+    files = "files2"
+
+    print("makeMeStrong_")
+    print(run_verif_strong(makeMeStrong_, files))
