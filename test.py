12d6e7a3747221d8202cf5211e64b892e3303fe1
Michel ZHANG <michel529020@gmail.com>
2022-03-17 14:51:21 +0100

    test.py

--- test.py
+++ test.py
@@ -2,7 +2,10 @@ from login_makeMeStrong import *
 from testlib import *
 
 if __name__ == '__main__':
-    files = "files2"
+    files = "files"
 
     print("makeMeStrong_")
     print(run_verif_strong(makeMeStrong_, files))
+
+    print("raghavan")
+    print(run_verif_strong(raghavan, files))
